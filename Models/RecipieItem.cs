c405bc422f1076e831ee4a120b8d6ba20a74319c
RitikRaj2021 <88115183+RitikRaj2021@users.noreply.github.com>
2022-11-15 19:23:23 +1300

    Updatetd the models table, added new entry field

--- Models/RecipieItem.cs
+++ Models/RecipieItem.cs
@@ -12,13 +12,10 @@ namespace App1.Models
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Steps { get; set; }
-        public string Steps2 { get; set; }
-        public string Steps3 { get; set; }
-        public string Steps4 { get; set; }
-        public string Steps5 { get; set; }
         public string Ingridients { get; set; }
         public string Name { get; set; }
         public string Notes { get; set; }
-     
+        public string CreatedByName { get; set; }
+
     }
 }
